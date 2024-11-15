48ed20ff73337414ab7dbd1f4f1bb3d6b68a5c5e
irzadzulhika29 <irjajulhika@gmail.com>
2024-11-15 23:45:01 +0700

    first commit

--- src/pages/dashboard/Profile.jsx
+++ src/pages/dashboard/Profile.jsx
@@ -1,8 +1,16 @@
 import Sidebar from "../../components/dashboard/Sidebar";
 import Navbar from "../../components/dashboard/Navbar";
 import userData from "../../assets/user/userData.json";
+import { useState } from 'react';
+import HistoryCard from "../../components/dashboard/profile/HistoryCard";
 
 const Profile = () => {
+    const [selectedNav, setSelectedNav] = useState('Riwayat Grab Meals');
+
+    const handleNavClick = (nav) => {
+        setSelectedNav(nav);
+    };
+
     return (
         <div className="flex min-h-screen">
             <Sidebar />
@@ -15,32 +23,80 @@ const Profile = () => {
                     {/* content */}
                     <section className="mx-10 my-5">
                         <div className="p-5 min-h-screen w-full bg-white shadow-md rounded-xl ">
-                            <div className="flex flex-col items-center text-center flex justify-center">
+                            <div className="flex flex-col items-center text-center justify-center">
                                 <img className="w-36 h-36 object-cover rounded-full" src={userData.user.image_url} alt="" />
                                 <h1 className="mt-3 text-2xl font-semibold">{userData.user.name}</h1>
                                 <p className="text-[#45c517]">{userData.user.location}</p>
                                 <button className="bg-[#45c517] font-semibold mt-5 py-2 px-3 rounded-full text-white">Edit Profile</button>
                             </div>
 
+                            {/* navigate */}
                             <div className="mt-5 flex justify-between">
-                                <p className="hover:cursor-pointer border-b-2 border-[#45c517] pb-2 text-md font-semibold text-[#45c517]">Riwayat Grab Meals</p>
-                                <p className="hover:cursor-pointer text-md font-semibold text-[#45c517]">Riwayat Sharing Meals</p>
-                                <p className="hover:cursor-pointer text-md font-semibold text-[#45c517]">Riwayat Activity</p>
-                                <p className="hover:cursor-pointer text-md font-semibold text-[#45c517]">Riwayat Charity</p>
-                                <p className="hover:cursor-pointer text-md font-semibold text-[#45c517]">Riwayat Article</p>
+                                <p
+                                    onClick={() => handleNavClick('Riwayat Grab Meals')}
+                                    className={`hover:cursor-pointer pb-2 text-md font-semibold ${selectedNav === 'Riwayat Grab Meals' ? 'border-b-2 border-[#45c517] text-[#45c517]' : ''
+                                        }`}
+                                >
+                                    Riwayat Grab Meals
+                                </p>
+                                <p
+                                    onClick={() => handleNavClick('Riwayat Sharing Meals')}
+                                    className={`hover:cursor-pointer pb-2 text-md font-semibold ${selectedNav === 'Riwayat Sharing Meals' ? 'border-b-2 border-[#45c517] text-[#45c517]' : ''
+                                        }`}
+                                >
+                                    Riwayat Sharing Meals
+                                </p>
+                                <p
+                                    onClick={() => handleNavClick('Riwayat Activity')}
+                                    className={`hover:cursor-pointer pb-2 text-md font-semibold ${selectedNav === 'Riwayat Activity' ? 'border-b-2 border-[#45c517] text-[#45c517]' : ''
+                                        }`}
+                                >
+                                    Riwayat Activity
+                                </p>
+                                <p
+                                    onClick={() => handleNavClick('Riwayat Charity')}
+                                    className={`hover:cursor-pointer pb-2 text-md font-semibold ${selectedNav === 'Riwayat Charity' ? 'border-b-2 border-[#45c517] text-[#45c517]' : ''
+                                        }`}
+                                >
+                                    Riwayat Charity
+                                </p>
+                                <p
+                                    onClick={() => handleNavClick('Riwayat Article')}
+                                    className={`hover:cursor-pointer pb-2 text-md font-semibold ${selectedNav === 'Riwayat Article' ? 'border-b-2 border-[#45c517] text-[#45c517]' : ''
+                                        }`}
+                                >
+                                    Riwayat Article
+                                </p>
                             </div>
-
-                            <div className="mt-5 p-4 w-full pb-5 bg-white shadow-md rounded-xl">
-                                <p className="font-semibold">01 Oktober 2024</p>
-                                <div>
-                                    <img className="w-32 h-20 object-cover rounded-md" src="https://thefamilydinnerproject.org/wp-content/uploads/2014/07/Sunday-roast-chicken2.jpg" alt="" />
-                                </div>
+                            {/* history card */}
+                            <div className="mt-5">
+                                {selectedNav === 'Riwayat Grab Meals' && (
+                                    <div className="p-4 w-full pb-5 bg-white shadow-md rounded-xl transform transition-transform duration-300 hover:-translate-y-2">
+                                       <HistoryCard />
+                                    </div>
+                                )}
+                                {selectedNav === 'Riwayat Sharing Meals' && (
+                                    <div className="p-4 w-full pb-5 bg-white shadow-md rounded-xl">
+                                        {/* Konten untuk Riwayat Sharing Meals */}
+                                    </div>
+                                )}
+                                {selectedNav === 'Riwayat Activity' && (
+                                    <div className="p-4 w-full pb-5 bg-white shadow-md rounded-xl">
+                                        {/* Konten untuk Riwayat Activity */}
+                                    </div>
+                                )}
+                                {selectedNav === 'Riwayat Charity' && (
+                                    <div className="p-4 w-full pb-5 bg-white shadow-md rounded-xl">
+                                        {/* Konten untuk Riwayat Charity */}
+                                    </div>
+                                )}
+                                {selectedNav === 'Riwayat Article' && (
+                                    <div className="p-4 w-full pb-5 bg-white shadow-md rounded-xl">
+                                        {/* Konten untuk Riwayat Article */}
+                                    </div>
+                                )}
                             </div>
-
-
-
                         </div>
-
                     </section>
                 </div>
             </section>
